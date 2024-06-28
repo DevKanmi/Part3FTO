9b68071ed56a0a5a44b51c9240f1eec48d95ed9f
Akinbayo Clinton <clintexakinbs@gmail.com>
2024-06-28 16:22:25 +0100

    update

--- index.js
+++ index.js
@@ -47,13 +47,34 @@ app.delete('/api/notes/:id',(request,response)=>{
   response.status(204).end() //error thrown if we try to access an element that has already been deleted
 })
 
+const generateId =() => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
 app.post('/api/notes', (request, response)=>{
-  const note = request.body
-  console.log(note)
+  const body = request.body
+  
+  if(!body.content){
+    return response.status(400).json({
+      error : "content missing"
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
   response.json(note)
 })
 
 })
 const PORT = 3001
 app.listen(PORT)
-console.log('server running on port ${PORT}')
+console.log(`server running on port ${PORT}`)
