eff75a222de9250ba7894839e3e32e15f91b8e41
Akinbayo Clinton <clintexakinbs@gmail.com>
2024-07-08 17:47:56 +0100

    update

--- mongo.js
+++ mongo.js
@@ -16,17 +16,43 @@ mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
   content: String,
-  important: Boolean,
+  important: Boolean,   // Create your node model schema 
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const Note = mongoose.model('Note', noteSchema) // Create your model based on the schema created
 
-const note = new Note({
-  content: 'HTML is easy',
+const note1 = new Note({
+  content: 'HTML is easy',   //Add objects to database using the Note(the note serves as a class blueprint) the "note" is an object created based on model.
   important: true,
 })
 
-note.save().then(result => {
-  console.log('note saved!')
-  mongoose.connection.close() //Needed for a program to finish execution.
+const note2 = new Note({
+  content: "JS is beans",
+  important: true,
+})
+
+const note3 = new Note({
+  content: "CSS Beautifies",
+  important: true,
+})
+
+
+// note1.save().then(result => {  // .save is to ensure the note is saved, the "then" indicated that after it is saved a function should be returned indicating the model has been saved ad then end connection
+//   console.log('note1 saved!')
+// })
+
+// note2.save().then(result => {  // .save is to ensure the note is saved, the "then" indicated that after it is saved a function should be returned indicating the model has been saved ad then end connection
+//   console.log('note2 saved!')
+// })
+
+// note3.save().then(result => {  // .save is to ensure the note is saved, the "then" indicated that after it is saved a function should be returned indicating the model has been saved ad then end connection
+//   console.log('note3 saved!')
+//   mongoose.connection.close() //Needed for a program to finish execution.
+// })
+
+Note.find({}).then(result =>{
+  result.forEach(note =>{
+    console.log(note);
+  }) //Used to Print all the Items currently stored in the database
+ mongoose.connection.close()
 })
