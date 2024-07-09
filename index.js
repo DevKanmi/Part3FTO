9f7ce2bf97a63e9fa4d91a7a99dccbb23c3037eb
Akinbayo Clinton <clintexakinbs@gmail.com>
2024-07-09 09:27:32 +0100

    update

--- index.js
+++ index.js
@@ -1,32 +1,72 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+// const mongoose = require('mongoose')
+
+
+// if (process.argv.length<3) {
+//   console.log('give password as argument')
+//   process.exit(1)
+// }
+
+// const password = process.argv[2]
+
+// const url = process.env.MONGODB_URI;
+ 
+// mongoose.set('strictQuery',false)
+
+// mongoose.connect(url)
+
+// const noteSchema = new mongoose.Schema({
+//   content: String,
+//   important: Boolean,   // Create your node model schema 
+// })
+
+// const Note = mongoose.model('Note', noteSchema) // Create your model based on the schema created
+
+// noteSchema.set('toJSON', {
+//   transform: (document, returnedObject) => {
+//     returnedObject.id = returnedObject._id.toString()
+//     delete returnedObject._id
+//     delete returnedObject.__v
+//   }
+// })
+
+const Note = require('./models/note')
+
+
 const express = require('express');
 const app = express()
 
 app.use(express.json());
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
+// let notes = [
+//     {
+//       id: 1,
+//       content: "HTML is easy",
+//       important: true
+//     },
+//     {
+//       id: 2,
+//       content: "Browser can execute only JavaScript",
+//       important: false
+//     },
+//     {
+//       id: 3,
+//       content: "GET and POST are the most important methods of HTTP protocol",
+//       important: true
+//     }
+//   ]
  
 app.get('/',(request,response) => {
     response.send('<h1>Hello World</h1>') //Route 1 defines an event handler
 })
 
 app.get('/api/notes',(request,response) =>{
-    response.json(notes) 
-})
+  Note.find({}).then(notes=>{
+      response.json(notes)
+  })
+  })
 
 app.get('/api/notes/:id',(request,response) =>{
   const id = Number(request.params.id)
@@ -75,6 +115,7 @@ app.post('/api/notes', (request, response)=>{
 })
 
 })
-const PORT = 3001
-app.listen(PORT)
-console.log(`server running on port ${PORT}`)
+const PORT = process.env.PORT
+app.listen(PORT,()=>{
+  console.log(`server running on port ${PORT}`)
+})
